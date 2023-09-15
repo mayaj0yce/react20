0677d6587326a1f69138f48baad97c3336ec8ded
mayaj0yce <m.j.thayne@gmail.com>
2023-09-14 21:38:41 -0600

    bio added

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -9,8 +9,11 @@ const AboutMe = () => {
     <div>
         <p>About me</p>
         <img src="" alt="" />
+        <p>Utah born and raised. Having been a part of healthcare for the last 8 years I am happy to be coding and in an environment as sterile as I choose it to be. I have two cats, Lilith and Ivy May. </p>
+        <img src="" alt="Ivy May" />
+        <img src="" alt="lil Devil"/>
     </div>
-  )
+  ) 
 }
 
 export default AboutMe
