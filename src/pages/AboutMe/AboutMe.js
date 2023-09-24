49e6f1701b1bd36e132e1f1d01cba241d6ff9b13
mayaj0yce <m.j.thayne@gmail.com>
2023-09-24 16:02:04 -0600

    about me photos

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './AboutMe.css'
+import me from '../../images/handsomeMan.jpg'
+import lilith from '../../images/lilith.jpg'
+import ivy from '../../images/ivyMay.JPG'
 
 
 const AboutMe = () => {
@@ -9,7 +12,7 @@ const AboutMe = () => {
         <p>About me</p>
         <div className='block columns'>
           <div className='column is-half'>
-            <img src= 'http://localhost:3000/public/img/handsomeMan.jpg' alt="self" />
+            <img src={me} alt="self" />
           </div>
           <div className='block column is-half'>
             <p>Hiya my name is MayaJ0yce. I was born and raised in Utah, currently located in Bluffdale. My previous work experience has been in health care, I am now transitioning into the tech world to experience less trauma and shit (literally). My house is full of art, mostly my own, and cat hair from my two cats, Ivy and Lilith</p>
@@ -17,11 +20,11 @@ const AboutMe = () => {
         </div>
         <div className='block columns'>
           <div className='column is-half'>
-            <img src='http://localhost:3000/public/img/ivyMay.jpg' alt="Ivy May" />
+            <img src={ivy} alt="Ivy May" />
             <p>IvyMay</p>
             </div>
           <div className='column is-half'>
-            <img src='http://localhost:3000/img/lilith.jpg' alt="lil Devil" />
+            <img src={lilith} alt="lil Devil" />
           <p> demon child Lilith </p>
           </div>
         </div>
