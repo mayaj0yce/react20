6296923f377c8a014518feb28022c8c8d168db21
mayaj0yce <m.j.thayne@gmail.com>
2023-09-24 17:30:03 -0600

    console log the img with import ''from'' and then add the console log result as the img prop

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,10 +4,10 @@ import './Project.css'
 export const Project = (props) => {
   return (
     <div className='block'>
-    
-<img className='imgClass' src={props.image} alt=''></img>
+    {/* className='imgClass'  */}
+<img src={props.image} alt={props.alt}></img>
 <div>{props.name}</div>
-<a href={props.github} target='_blank'> GITHUB </a>
+<a href={props.github} target='_blank' rel='noreferrer'> GITHUB </a>
 <div>{props.description}</div>
     </div>
   )
--- src/pages/MyWork/MyWork.js
+++ src/pages/MyWork/MyWork.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import 'bulma/css/bulma.min.css';
 import Project from '../../components/Project/Project';
-// import portfolioImg from '../../img/portfolio.jpg';
+import portfolio from '../../images/portfolio.jpg';
+// import from '';
+// import from '';
+// import from '';
+// import from '';
+
+
 // import { useHref } from 'react-router-dom';
 
 // For each project that is featured in your portfolio, include the following:
@@ -11,31 +17,36 @@ import Project from '../../components/Project/Project';
 // * The title of the project
 const projects = [
 {
-  image:  'http://localhost:3000/public/img/portfolio.jpg' ,
+  image: '/react20/static/media/portfolio.3f7fd98c5f6af95eedad.jpg',
+  alt: 'portfolio Img',
   name: 'Portfolio',
   github: 'https://github.com/mayaj0yce/professional-portfolio.git',
   description: 'My first portfolio created from scratch',
 },
 {
-  image:  'http://localhost:3000/img/mockUp.jpg',
+  image:  'public/images/mockUp.jpg',
+  alt: 'Horiseon Mock Img',
   name: 'Horiseon',
   github: 'https://github.com/mayaj0yce/Horiseon-work-up-.git',
   description: 'HTML and CSS work up for a fake company demo',
 },
 {
   image: 'http://localhost:3000/img/ReadMe.jpg',
+  alt: 'Read Me Img',
   name: 'Read Me Application',
   github: 'https://github.com/mayaj0yce/readMeRights.git',
   description: 'Creates a Readme based on a users input',
 },
 {
   image:  'http://localhost:3000/img/artData.jpg',
+  alt: 'Art Data Img',
   name: 'ArtGallorey',
   github: 'https://github.com/mayaj0yce/artGallorey.git',
   description: 'class project 2. I was alone and did the back end for an art gallery site',
 },
 {
   image:  'http://localhost:3000/img/workDay.jpg',
+  alt: 'Work Day Img',
   name: 'Work Day Scheduler',
   github: 'https://github.com/mayaj0yce/workDay.git',
   description: 'A hectic work day can be easily solved by using this day planner app that tracks by the hour and updates as time passes through the day.',
@@ -43,6 +54,7 @@ const projects = [
 
 ]
 // * A link to the deployed application
+console.log(portfolio)
 
 // * A link to the corresponding GitHub repository
 
